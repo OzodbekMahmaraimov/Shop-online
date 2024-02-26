da214b29ba4800d0d0fce5927c447b46693ba4ab
ASILEBEK09 <you@example.com asilbeknusratov2@gmail.com>
2024-02-26 21:10:19 +0500

    Sign Up tayyor

---
+++ src/components/Asilbek/Sign/SignUp.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import telefon from '../images/telefon.png';
+import '../Sign/Sign.css';
+import google from '../images/google.png'
+import {Button} from 'reactstrap'
+function SignUp() {
+    return (  
+        <>
+            <img width={600}height={640} className="telefon" src={telefon} alt="" />
+            <div className="acaunt">
+                <h1>Create an account</h1>
+                <h6>Enter your details below</h6>
+               <div className="Firtname1">
+               <input className="Fistname" placeholder="First name"/>
+                <hr  />
+               </div>
+               <div className="lastname">
+               <input className="Fistname" placeholder="Last_name"/>
+                <hr  />
+               </div>
+               <div className="Email">
+               <input type="email" className="Fistname" placeholder="Email"/>
+                <hr  />
+               </div>
+               <div className="phone_number">
+               <input type="number" className="Fistname" placeholder="phone_number"/>
+                <hr  />
+               </div>
+               <div className="password">
+               <input type="password" className="Fistname" placeholder="password"/>
+                <hr  />
+               </div>
+               <Button color="danger"className="Accc" >Create Account</Button>
+               <button className="regis"><img src={google} alt="" /> Sign Up with Google</button>
+            </div>
+        </>
+    );
+}
+
+export default SignUp;
