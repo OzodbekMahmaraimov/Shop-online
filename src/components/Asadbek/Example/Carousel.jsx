8006eb4310730ff3a262896406ee5b067a129771
OzodbekMahmaraimov <ozodebkmahmaraimov07@gmail.com>
2024-02-29 20:41:20 +0500

    Asadning carouselini to'g'irladim

--- src/components/Asadbek/Example/Carousel.jsx
+++ src/components/Asadbek/Example/Carousel.jsx
@@ -1,93 +1,35 @@
-import React, { useState } from 'react';
-import Carousel1 from '../imgs/rasm1.png'
+import Carousel from 'react-bootstrap/Carousel';
 
-
-import {
-  Carousel,
-  CarouselItem,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselCaption,
-} from 'reactstrap';
-
-
-const items = [
-  {
-    src: {Carousel1},
-    key: 1,
-  },
-  {
-    src: {Carousel1},
-    key: 2,
-  },
-  {
-    src: {Carousel1},
-    key: 3,
-  },
-];
-
-function Example(args) {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [animating, setAnimating] = useState(false);
-
-  const next = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-    setActiveIndex(nextIndex);
-  };
-
-  const previous = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-    setActiveIndex(nextIndex);
-  };
-
-  const goToIndex = (newIndex) => {
-    if (animating) return;
-    setActiveIndex(newIndex);
-  };
-
-  const slides = items.map((item) => {
-    return (
-      <CarouselItem
-        onExiting={() => setAnimating(true)}
-        onExited={() => setAnimating(false)}
-        key={item.src}
-      >
-        <img src={Carousel1} alt={item.altText} />
-        <CarouselCaption
-          captionText={item.caption}
-          captionHeader={item.caption}
-        />
-      </CarouselItem>
-    );
-  });
-
+const CarouselSlider = () => {
   return (
-    <Carousel
-      activeIndex={activeIndex}
-      next={next}
-      previous={previous}
-      {...args}
-    >
-      <CarouselIndicators
-        items={items}
-        activeIndex={activeIndex}
-        onClickHandler={goToIndex}
-      />
-      {slides}
-      <CarouselControl
-        direction="prev"
-        directionText="Previous"
-        onClickHandler={previous}
-      />
-      <CarouselControl
-        direction="next"
-        directionText="Next"
-        onClickHandler={next}
-      />
-    </Carousel>
-  );
+    <>
+      <Carousel style={{height: "100vh"}} className='w-100'>
+        <Carousel.Item>
+          <img style={{objectFit: "cover"}} className='w-100' src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" alt="..." />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img style={{objectFit: "cover"}} className='w-100' src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" alt="..." />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img style={{objectFit: "cover"}} className='w-100' src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" alt="..." />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  )
 }
 
-export default Example;
+export default CarouselSlider
--- src/components/Asadbek/Example/Yozuvlar.jsx
+++ src/components/Asadbek/Example/Yozuvlar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Yozuvlar = () => {
   return (
    <>
-   <div>asas</div>
+   <div></div>
 
    </>
   )
